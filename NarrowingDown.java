bca61d0a0af7171aafa7b851457a6e34a60c18fb
RutujaBandgar <84956687+RutujaBandgar@users.noreply.github.com>
2022-07-14 16:38:33 +0530

    Add files via upload

---
+++ NarrowingDown.java
@@ -0,0 +1,10 @@
+class NarrowingDown
+{
+	public static void main(String a[])
+	{
+		double myDouble = 10.46;
+		int myInt = (int) myDouble;
+		System.out.println(myDouble);
+		System.out.println(myInt);
+	}
+}
