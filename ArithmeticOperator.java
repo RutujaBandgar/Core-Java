fe43f4d6cd6111f3093a72963695fa16adcd863c
RutujaBandgar <84956687+RutujaBandgar@users.noreply.github.com>
2022-07-15 18:09:18 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,18 @@
+class ArithmeticOperator
+{
+	public static void main(String args[])
+	{
+		int a=20;
+		int b=10;
+		System.out.print("Addition od a and b :");
+		System.out.println(a+b);
+		System.out.print("Substraction od a and b :");
+		System.out.println(a-b);
+		System.out.print("Multiplication od a and b :");
+		System.out.println(a*b);
+		System.out.print("Division od a and b :");
+		System.out.println(a/b);
+		System.out.print("Modulus od a and b :");
+		System.out.println(a%b);
+	}
+}
