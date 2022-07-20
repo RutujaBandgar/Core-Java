f9cbe2f0ba51c6841cdac6b1cf9204d8abf63179
RutujaBandgar <84956687+RutujaBandgar@users.noreply.github.com>
2022-07-20 19:07:53 +0530

    Add files via upload

---
+++ ForLoop1.java
@@ -0,0 +1,11 @@
+class ForLoop1
+{
+   public static void main(String[]args)
+   {
+    	System.out.println("Value from 11 to 3");
+    	for(int i=11;i>3;i--)
+     	{
+        	System.out.println("values:"+ i);
+      	}
+   }
+}
